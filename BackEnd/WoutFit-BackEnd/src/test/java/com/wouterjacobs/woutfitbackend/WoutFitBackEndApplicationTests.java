6351b5d269af07b92f90f0111b219a1d1525eee5
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-07-11 00:05:41 +0200

    init of the Java Springboot backend

---
+++ BackEnd/WoutFit-BackEnd/src/test/java/com/wouterjacobs/woutfitbackend/WoutFitBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wouterjacobs.woutfitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoutFitBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
