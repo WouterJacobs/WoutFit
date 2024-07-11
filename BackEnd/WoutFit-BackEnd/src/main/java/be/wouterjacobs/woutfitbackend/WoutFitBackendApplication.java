1de12048d354a2fbb4655c530959c1bdddf897bc
WouterJacobs <wouterwasalbezet@hotmail.com>
2024-07-11 14:33:06 +0200

    new init of Backend part 2

---
+++ BackEnd/WoutFit-BackEnd/src/main/java/be/wouterjacobs/woutfitbackend/WoutFitBackendApplication.java
@@ -0,0 +1,13 @@
+package be.wouterjacobs.woutfitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoutFitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoutFitBackendApplication.class, args);
+	}
+
+}
---
+++ BackEnd/WoutFit-BackEnd/src/test/java/be/wouterjacobs/woutfitbackend/WoutFitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.wouterjacobs.woutfitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoutFitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
